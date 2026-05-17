51e86046a77c59dababaa5c62de579bce4112d02
halilatakol <halilatakol@gmail.com>
2026-05-17 15:06:29 +0300

    fix: tabIndicatorOffset replace with manual offset

--- feature/calendar/src/main/kotlin/com/dersium/feature/calendar/CalendarScreen.kt
+++ feature/calendar/src/main/kotlin/com/dersium/feature/calendar/CalendarScreen.kt
@@ -3,6 +3,7 @@ package com.dersium.feature.calendar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
