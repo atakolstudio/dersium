49d8e89de61dc8d628cf0d5b8ec7f9135f015381
halilatakol <halilatakol@gmail.com>
2026-05-22 13:44:22 +0300

    feat: Room schema export, production ready

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
     alias(libs.plugins.ksp)
     alias(libs.plugins.room)
 }
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
+}
+
 android {
     namespace = "com.dersium.core.database"
     compileSdk = 36
